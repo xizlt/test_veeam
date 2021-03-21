d050c209d2f72cfa9c862a19a3e83786fa00ba75
xizlt <37120642+xizlt@users.noreply.github.com>
2021-03-22 01:04:57 +0300

    manual test

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+from pathlib import Path
 
 COLUMNS_IN_ROW = 3
 HASH_TYPE = ['md5', 'sha1', 'sha256']
@@ -35,13 +36,14 @@ class CheckHash:
         file = self.path_to_files + separate_line[0]
         if len(separate_line) != COLUMNS_IN_ROW:
             """ Check the allowed number of columns in a row"""
-            print(f"Invalid string \n--- {line} ---\n in line {num_line}. Max count columns: {COLUMNS_IN_ROW}")
+            print(f"Invalid string \n--- {line} ---\n in line {num_line}. Count columns must be: {COLUMNS_IN_ROW}")
             exit()
         else:
             if separate_line[1].lower() not in HASH_TYPE:
                 """ Check if the hash name is in the available list """
                 print(
-                    f"\nInvalid encoding method: {separate_line[1].lower()}\n--- {line} ---\nin line {num_line}. Available encoding: {HASH_TYPE}")
+                    f"\nInvalid encoding method: {separate_line[1].lower()}\n--- {line} ---\nin line {num_line}"
+                    f". Available encoding: {HASH_TYPE}")
                 exit()
 
             if len(separate_line[0].split('.')) < 2 or separate_line[0].split('.')[0] == '':
@@ -52,6 +54,8 @@ class CheckHash:
                 if not os.path.exists(file):
                     """ Check available file"""
                     print(f'{file} NOT FOUND')
+                    return False
+
             return True
 
     def _check_hash(self, obj):
@@ -81,13 +85,16 @@ class CheckHash:
         :return: None
         """
         try:
+            if Path(self.path_input_file).stat().st_size == 0:
+                print(f"File {self.path_input_file} empty")
+                exit()
             with open(self.path_input_file, "r") as f:
                 for num, line in enumerate(f, 1):
                     if len(line.split()) == 0:
                         continue
                     str_line = line.replace('\n', '')
                     if not self._validation_line_in_file(str_line, num):
-                        break
+                        continue
                     else:
                         self._check_hash(str_line)
         except FileNotFoundError:
